8131ab59dbade52737699dbd611de881adbb3e67
Jean Port <pierreanatc522@gmail.com>
2023-06-21 15:02:07 -0300

    DatabaseException

---
+++ src/main/java/com/educandoweb/course/servicies/exeption/DataBaseException.java
@@ -0,0 +1,12 @@
+package com.educandoweb.course.servicies.exeption;
+
+public class DataBaseException extends RuntimeException{
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	public DataBaseException(String msg) {
+		super(msg);
+	}
+
+}
